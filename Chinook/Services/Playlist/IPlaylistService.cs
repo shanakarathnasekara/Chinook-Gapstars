395fedf00ba4124a5879ff36cc75454d8c1eefc8
Shanaka Rathnasekara <shanakar@99x.io>
2023-07-16 12:19:59 +0530

    Functionality implementation for adding to a playlist, searching on artist page and adding tracks to a specific playlist

--- Chinook/Services/Playlist/IPlaylistService.cs
+++ Chinook/Services/Playlist/IPlaylistService.cs
@@ -1,5 +1,6 @@
 ﻿using Chinook.ClientModels;
 using Chinook.Models;
+using NuGet.DependencyResolver;
 
 namespace Chinook.Services.Playlist
 {
@@ -8,5 +9,9 @@ namespace Chinook.Services.Playlist
         Task<Track> AddTrackToFavorites(long trackId);
         Task<Track> RemoveTrackFromFavorites(long trackId);
         Task<List<PlaylistTrack>> RetrieveTracksList(long artistId, string currentUserId);
+        Task<List<Models.Playlist>> RetrieveUsersListofPlaylist(string currentUserId);
+        Task AddNewPlaylist(PlaylistTrack selectedTrack, string newPlaylistName, string currentUserId);
+        Task RemoveTrackFromPlaylist(long trackId, string playlistName);
+        Task AddTrackToPlaylist(PlaylistTrack selectedTrack, string newPlaylistName);
     }
 }
--- Chinook/Services/Playlist/PlaylistService.cs
+++ Chinook/Services/Playlist/PlaylistService.cs
@@ -56,5 +56,59 @@ namespace Chinook.Services.Playlist
 
             return tracks;
         }
+
+        public async Task<List<Models.Playlist>> RetrieveUsersListofPlaylist(string currentUserId)
+        {
+            var userPlaylists = await _dbContext.Playlists.Include(p => p.UserPlaylists).Where(p => p.UserPlaylists.Any(up => up.UserId == currentUserId && up.Playlist.Name != "Favorites")).ToListAsync();
+            return userPlaylists;
+        }
+
+        public async Task AddNewPlaylist(PlaylistTrack selectedTrack, string newPlaylistName, string currentUserId)
+        {
+            Models.Playlist playlist = new Models.Playlist()
+            {
+                Name = newPlaylistName,
+                PlaylistId = _dbContext.Playlists.OrderBy(a => a.PlaylistId).Last().PlaylistId + 1
+            };
+
+            _dbContext.Playlists.Add(playlist);
+            await _dbContext.SaveChangesAsync();
+
+
+            var track = _dbContext.Tracks.FirstOrDefault(t => t.TrackId == selectedTrack.TrackId);
+            var trackData = await _dbContext.Tracks.Where(t => t.TrackId == selectedTrack.TrackId).Include(a => a.Playlists).FirstOrDefaultAsync();
+            var favoritePlaylist = await _dbContext.Playlists.Where(p => p.Name == newPlaylistName).FirstOrDefaultAsync();
+            trackData.Playlists.Add(favoritePlaylist);
+            _dbContext.Tracks.Update(trackData);
+
+            UserPlaylist userPlaylist = new UserPlaylist()
+            {
+                UserId = currentUserId,
+                PlaylistId = playlist.PlaylistId
+            };
+            _dbContext.UserPlaylists.Add(userPlaylist);
+            await _dbContext.SaveChangesAsync();
+
+        }
+
+        public async Task RemoveTrackFromPlaylist(long trackId, string playlistName)
+        {
+            Track track = await _dbContext.Tracks.Where(t => t.TrackId == trackId).Include(a => a.Playlists).FirstOrDefaultAsync();
+            Models.Playlist playList = await _dbContext.Playlists.Where(p => p.Name == playlistName).FirstOrDefaultAsync();
+
+            track.Playlists.Remove(playList);
+            _dbContext.Tracks.Update(track);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task AddTrackToPlaylist(PlaylistTrack selectedTrack, string playlistName)
+        {
+            var trackData = await _dbContext.Tracks.Where(t => t.TrackId == selectedTrack.TrackId).Include(a => a.Playlists).FirstOrDefaultAsync();
+            var playList = await _dbContext.Playlists.Where(p => p.Name == playlistName).FirstOrDefaultAsync();
+            trackData.Playlists.Add(playList);
+            _dbContext.Tracks.Update(trackData);
+            await _dbContext.SaveChangesAsync();
+        }
+
     }
 }
